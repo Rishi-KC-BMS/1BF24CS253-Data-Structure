aa54c9d184afcb865f19c0559ea524c7c5002e16
Rishi Kumar Chourasia <rishikumar.cs24@bmsce.ac.in>
2025-12-01 09:33:56 +0530

    Update main.c

--- Doubly Linked List/main.c
+++ Doubly Linked List/main.c
@@ -38,6 +38,25 @@ struct node* insert_left(struct node* head, int value) {
 
     return newnode;
 }
+struct node* insert_right(struct node* head, int value) {
+    struct node* newnode = malloc(sizeof(struct node));
+    newnode->data = value;
+    newnode->prev = NULL;
+    newnode->next = NULL;
+
+    if (head == NULL)
+        return newnode;
+
+    struct node* temp = head;
+    while (temp->next != NULL)
+        temp = temp->next;
+
+    temp->next = newnode;
+    newnode->prev = temp;
+
+    return head;
+}
+
 
 struct node* delete_value(struct node* head, int value) {
     struct node* temp = head;
@@ -77,7 +96,7 @@ int main() {
     struct node* head = NULL;
     int choice, val;
 
-    printf("\n1. Create Node \n2. Insert at Left\n3. Delete Value\n4. Display\n5. Exit\n");
+    printf("\n1. Create Node \n2. Insert at Left \n3. Insert at Right \n4. Delete Value\n5. Display\n6. Exit\n");
 
     while (1) {
         printf("\nEnter choice : ");
@@ -93,17 +112,22 @@ int main() {
             scanf("%d", &val);
             head = insert_left(head, val);
         }
-        else if (choice == 3) {
+          else if (choice == 3) {
+            printf("Enter value to insert : ");
+            scanf("%d", &val);
+            head = insert_right(head, val);
+        }
+        else if (choice == 4) {
             printf("Enter value to delete: ");
             scanf("%d", &val);
             head = delete_value(head, val);
         }
-        else if (choice == 4) {
+        else if (choice == 5) {
             printf("List: ");
             display(head);
 
         }
-         else if (choice == 5) {
+         else if (choice == 6) {
             break;
 
         }
